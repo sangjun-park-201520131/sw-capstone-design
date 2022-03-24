fe7751f9162df43b0ad826f2f38e601dcbbfd973
sangjun-park-201520131 <101028399+sangjun-park-201520131@users.noreply.github.com>
2022-03-24 13:52:04 +0900

    merge

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +24,30 @@ function App() {
 }
 
 export default App;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> parent of 04396ab (Merge pull request #3 from sangjun-park-201520131/sangjun-park-201520131)
--- client/myapp/src/App.test.js
+++ client/myapp/src/App.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,3 +7,13 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+=======
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+>>>>>>> parent of 04396ab (Merge pull request #3 from sangjun-park-201520131/sangjun-park-201520131)
