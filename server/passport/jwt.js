ea13ae661eaf6e4d02b05e5a14eed217ee53fb7c
SeungheonShin <gody8756@ajou.ac.kr>
2022-03-24 12:17:33 +0900

    [feat] 회원가입 및 로그인 / 구글 및 jwt 인증방식 추가

---
+++ server/passport/jwt.js
@@ -0,0 +1,32 @@
+
+//bearer token 인증
+
+const passport = require('passport');
+const { User } = require('../models');
+const { ExtractJwt, Strategy: JWTStrategy } = require('passport-jwt');
+
+module.exports = ()  => {
+    passport.use(new JWTStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('authorization'),
+        secretOrKey: process.env.JWT_SECRET_KEY,
+    }, async(jwtPayload, done) => {
+        try {
+            console.log(`jwt email :${jwtPayload.email}`);
+            const user = await User.findOne({
+                where: {
+                    userID : jwtPayload.email
+                }
+            });
+            if(user) {
+                return done(null, user);
+            }
+            else {
+                done(null, false, { reason: '잘못된 인증정보 입니다' });
+            }
+        } catch(err) {
+            console.log(err);
+            done(err);
+        }
+    }))
+};
+
