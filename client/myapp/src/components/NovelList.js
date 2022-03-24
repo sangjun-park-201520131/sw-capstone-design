156e4dc939f3c38ebdfa793a366c8fbd87e7fd86
sangjun-park-201520131 <101028399+sangjun-park-201520131@users.noreply.github.com>
2022-03-24 13:47:35 +0900

    merge

---
+++ client/myapp/src/components/NovelList.js
@@ -0,0 +1,19 @@
+import NovelItem from "./NovelItem";
+
+const NovelList = ({ title, value }) => {
+  console.log(value);
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {value.map((novelData, idx) => (
+          <li key={idx}>
+            <NovelItem data={novelData} ilstId={idx} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default NovelList;
